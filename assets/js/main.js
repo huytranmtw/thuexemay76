739552ab7a25be4cf1bf857f349b4f6743e82d86
huytranmtw <giaydepahuy@gmail.com>
2026-08-23 11:04:56 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-const menuBtn=document.querySelector('.menu-toggle');const nav=document.querySelector('.main-nav');
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("booking-form");
   const status = document.getElementById("form-status");
